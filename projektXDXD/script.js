0d8163671719c66049dce95eff083c9b17f6dd38
Zao32852 <troolekt@gmail.com>
2025-12-17 13:04:47 +0100

    dodanie waznych funkcji

--- projektXDXD/script.js
+++ projektXDXD/script.js
@@ -55,4 +55,105 @@ const habitList = document.getElementById("habitList");
         }
 
     }
+function getToday() {
+        return new Date().toISOString().split("T")[0];
+      }
+      habits.push({
+        name: habitName,
+        history: {}
+      });
+      function getLast7Days() {
+        const days = [];
+        for (let i = 6; i >= 0; i--) {
+          const date = new Date();
+          date.setDate(date.getDate() - i);
+          days.push(date.toISOString().split("T")[0]);
+        }
+        return days;
+      }
+      function renderHabits() {
+        habitList.innerHTML = "";
+      
+        const days = getLast7Days();
+      
+        habits.forEach((habit, habitIndex) => {
+          const li = document.createElement("li");
+      
+          let calendarHTML = `<div class="calendar">`;
+      
+          days.forEach(day => {
+            const done = habit.history[day];
+            calendarHTML += `
+              <div 
+                class="day ${done ? "done" : ""}" 
+                data-day="${day}"
+              ></div>
+            `;
+          });
+      
+          calendarHTML += `</div>`;
+      
+          li.innerHTML = `
+            <strong>${habit.name}</strong>
+            ${calendarHTML}
+          `;
+      
+          const dayElements = li.querySelectorAll(".day");
+      
+          dayElements.forEach((dayEl, dayIndex) => {
+            dayEl.addEventListener("click", () => {
+              toggleDay(habitIndex, days[dayIndex]);
+            });
+          });
+      
+          habitList.appendChild(li);
+        });
+      }
+      function toggleDay(habitIndex, day) {
+        const habit = habits[habitIndex];
+      
+        if (habit.history[day]) {
+          delete habit.history[day];
+        } else {
+          habit.history[day] = true;
+        }
+      
+        saveHabits();
+        renderHabits();
+      }
+        function calculateStreak(habit) {
+        let streak = 0;
+        let date = new Date();
+
+        while (true) {
+            const day = date.toISOString().split("T")[0];
+            if (habit.history[day]) {
+            streak++;
+            date.setDate(date.getDate() - 1);
+            } else {
+            break;
+            }
+        }
+
+        return streak;
+        }
+        function calculateStreak(habit) {
+            let streak = 0;
+            let date = new Date();
+        
+            while (true) {
+            const day = date.toISOString().split("T")[0];
+            if (habit.history[day]) {
+                streak++;
+                date.setDate(date.getDate() - 1);
+            } else {
+                break;
+            }
+            }
+        
+            return streak;
+        }
+        const streak = calculateStreak(habit);
+        <strong>${habit.name} 🔥 ${streak}</strong>
+
     loadHabits();
